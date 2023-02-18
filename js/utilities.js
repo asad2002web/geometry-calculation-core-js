1635e2b27df1867e894b475b0e6c284bcb4cfea0
asad2002web <asad2002web@gmail.com>
2023-02-18 23:18:22 +0600

    question added

--- js/utilities.js
+++ js/utilities.js
@@ -2,19 +2,24 @@
 document.getElementById('btn-blog').addEventListener('click', function(){
     window.open('./blog.html', '_blank');
 });
+
+// 
+
 // Function Re-use:
+
+// Input Value Function
 function getInputValue(inputId) {
     const getInput = document.getElementById(inputId);
     const getInputNumber = parseFloat(getInput.value);
     getInput.value = '';
-    if (isNaN(getInputNumber)) {
+    if (isNaN(getInputNumber) || getInputNumber < 1) {
         alert('Please provide a valid Number.')
         return getInput.value = '';
     }
     return getInputNumber;
 }
 
-// 
+// get Result InnerText Function
 
 function getInnerText(id, area){
     const areaTriangelResult = document.getElementById(id);
@@ -22,7 +27,7 @@ function getInnerText(id, area){
     return areaTriangelResult.innerText;
 };
 
-// 
+// display Show Function
 function displayShow(showId){
     const areaTriangelShowe = document.getElementById(showId);
     areaTriangelShowe.style.display = "block";

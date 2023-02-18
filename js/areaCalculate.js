e71591bacbc03d110bd423cc5ea736673809e190
asad2002web <asad2002web@gmail.com>
2023-02-18 22:51:26 +0600

    all function re-use added

--- js/areaCalculate.js
+++ js/areaCalculate.js
@@ -7,7 +7,7 @@ document
 
     const triangelArea = 0.5 * triangelFirstInput * triangelSecondInput;
     displayShow("triangel-area-show");
-    getInnerText("triangel-area" , triangelArea);
+    getInnerText("triangel-area", triangelArea);
   });
 
 //  Rectangular Area Calculate:
@@ -19,19 +19,24 @@ document
 
     const areaRectangular = rectangularFirstInput * rectangularSecondInput;
     displayShow("rectangular-area-show");
-    getInnerText("rectangular-area" , areaRectangular);
+    getInnerText("rectangular-area", areaRectangular);
   });
 
 // Parallelogram:
 document
   .getElementById("btn-parallelogram-calculate")
   .addEventListener("click", function () {
-    const parallelogramFirstInput = getInputValue("baseInputField-parallelogram");
-    const parallelogramSecondInput = getInputValue("height-input-field-parallelogram");
+    const parallelogramFirstInput = getInputValue(
+      "baseInputField-parallelogram"
+    );
+    const parallelogramSecondInput = getInputValue(
+      "height-input-field-parallelogram"
+    );
 
-    const areaParallelogram = parallelogramFirstInput * parallelogramSecondInput;
+    const areaParallelogram =
+      parallelogramFirstInput * parallelogramSecondInput;
     displayShow("parallelogram-area-show");
-    getInnerText("parallelogram-area" , areaParallelogram);
+    getInnerText("parallelogram-area", areaParallelogram);
   });
 
 //   Rhombus;
@@ -43,53 +48,29 @@ document
 
     const areaRhombus = 0.5 * rhombusFirstInput * rhombusSecondInput;
     displayShow("rhombus-area-show");
-    getInnerText("rhombus-area" , areaRhombus);
+    getInnerText("rhombus-area", areaRhombus);
   });
 
 //   Pentagon:
 document
   .getElementById("btn-pentagon-calculate")
   .addEventListener("click", function () {
-    const pInputField = document.getElementById("pInputField-pentagon");
-    const pInput = parseFloat(pInputField.value);
+    const pentagonFirstInput = getInputValue("pInputField-pentagon");
+    const pentagonSecondInput = getInputValue("bInputField-pentagon");
 
-    const bInputField = document.getElementById("bInputField-pentagon");
-    const bInput = parseFloat(bInputField.value);
-
-    if (isNaN (pInput) || isNaN(bInput)) {
-      alert("Please Write  a Number");
-    } else {
-      const areaPentagonShowe = document.getElementById("pentagon-area-show");
-      const pentagonResult = document.getElementById("pentagon-area");
-      const areaPentagon = 0.5 * pInput * bInput;
-      areaPentagonShowe.style.display = "block";
-      pentagonResult.innerText = areaPentagon.toFixed(2);
-
-      pInputField.value = "";
-      bInputField.value = "";
-    }
+    const areaPentagon = 0.5 * pentagonFirstInput * pentagonSecondInput;
+    displayShow("pentagon-area-show");
+    getInnerText("pentagon-area", areaPentagon);
   });
 
 // Ellipse:
 document
   .getElementById("btn-ellipse-calculate")
   .addEventListener("click", function () {
-    const aInputField = document.getElementById("aInputField-ellipse");
-    const aInput = parseFloat(aInputField.value);
+    const ellipseFirstInput = getInputValue("aInputField-ellipse");
+    const ellipseSecondInput = getInputValue("bInputField-ellipse");
 
-    const bInputField = document.getElementById("bInputField-ellipse");
-    const bInput = parseFloat(bInputField.value);
-
-    if (isNaN (aInput) || isNaN(bInput)) {
-      alert("Please Write  a Number");
-    } else {
-      const areaEllipseShowe = document.getElementById("ellipse-area-show");
-      const ellipseResult = document.getElementById("ellipse-area");
-      const areaEllipse = 3.14 * aInput * bInput;
-      areaEllipseShowe.style.display = "block";
-      ellipseResult.innerText = areaEllipse.toFixed(2);
-
-      aInputField.value = "";
-      bInputField.value = "";
-    }
+    const areaEllipse = 3.14 * ellipseFirstInput * ellipseSecondInput;
+    displayShow("ellipse-area-show");
+    getInnerText("ellipse-area", areaEllipse);
   });
